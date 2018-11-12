0834b535a9d5f7920bccbee70cafdc8d86ee2348
JM Bergqvist <jm.bergqvist@intem.se>
2018-11-12 17:59:50 +0100

    Initial support for creating ncx file

---
+++ src/main/java/se/intem/epub/NcxTask.java
@@ -0,0 +1,72 @@
+package se.intem.epub;
+
+import java.io.File;
+import java.io.InputStream;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.TaskAction;
+import org.slf4j.LoggerFactory;
+
+public class NcxTask extends DefaultTask {
+
+  /** Logger for this class */
+  private static final Logger log = (Logger) LoggerFactory.getLogger(NcxTask.class);
+
+  private File sourceDirectory;
+
+  @InputDirectory
+  public File getSourceDirectory() {
+    return sourceDirectory;
+  }
+
+  public void setSourceDirectory(File sourceDirectory) {
+    this.sourceDirectory = sourceDirectory;
+  }
+
+  @TaskAction
+  public void generate() {
+    log.lifecycle("Generating ncx...");
+    String navName = "EPUB/nav.xhtml";
+    File nav = new File(sourceDirectory, navName);
+
+    if (!nav.exists()) {
+      throw new GradleException("Could not find nav file " + navName);
+    }
+
+    String ncxName = "EPUB/toc.ncx";
+    File ncx = new File(sourceDirectory, ncxName);
+
+    log.debug("Generating ncx from source nav {} to {}", nav, ncx);
+
+    try {
+
+      Transformer transformer = createXslTransformer();
+
+      StreamSource source = new StreamSource(nav);
+      StreamResult result = new StreamResult(ncx);
+      transformer.transform(source, result);
+
+      log.lifecycle("Created " + ncxName);
+
+    } catch (TransformerException e) {
+      throw new GradleException("Failed to create ncx", e);
+    }
+  }
+
+  private Transformer createXslTransformer() throws TransformerConfigurationException {
+    InputStream xsl = NcxTask.class.getResourceAsStream("/navdoc2ncx.xsl");
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    StreamSource xslSource = new StreamSource(xsl);
+    Transformer transformer = transformerFactory.newTransformer(xslSource);
+    transformer.setParameter("cwd", sourceDirectory.getAbsolutePath() + "/");
+    return transformer;
+  }
+}
